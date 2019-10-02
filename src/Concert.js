23e3a21e545b605e7a670253b1f67b9d980d1fb8
marwehl <Marie_S@gmx.de>
2019-10-02 11:49:53 +0200

    Add styled icons

--- src/Concert.js
+++ src/Concert.js
@@ -3,7 +3,9 @@ import styled from 'styled-components/macro';
 import Tag from './Tag'
 import PropTypes from 'prop-types'
 import { AngleDoubleDown } from 'styled-icons/fa-solid/AngleDoubleDown'
-
+import { DateRange } from 'styled-icons/material/DateRange'
+import { Place } from 'styled-icons/material/Place'
+ 
 
 export default function Concert({ 
   artist, 
@@ -41,11 +43,11 @@ const [arrowShowsDown, setArrowShowsDown] = useState(false)
       <ArtistStyled>{artist}</ArtistStyled>
       <TimeStyled>
         <div>
-          <IconStyled aria-hidden="true" focusable="false" data-prefix="far" data-icon="calendar-alt" class="svg-inline--fa fa-calendar-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M148 288h-40c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12zm108-12v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 96v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm192 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96-260v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h48V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h128V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h48c26.5 0 48 21.5 48 48zm-48 346V160H48v298c0 3.3 2.7 6 6 6h340c3.3 0 6-2.7 6-6z"></path></IconStyled>
+          <DateRangeStyled />
       <span>{date}</span>
         </div>
         <div>
-            <IconStyled aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-marker-alt" class="svg-inline--fa fa-map-marker-alt fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></IconStyled>
+            <PlaceStyled />
       <span>{place}</span>
           </div>
       </TimeStyled>
@@ -104,9 +106,14 @@ justify-content: space-between;
 gap: 7px;
 `
 
-const IconStyled = styled.svg`
-width: 12px;
-margin: 0 5px;
+const DateRangeStyled = styled(DateRange)`
+width: 18px;
+margin: 0 5px 5px;
+`
+
+const PlaceStyled = styled(Place)`
+width: 18px;
+margin: 0 5px 5px;
 `
 
 const AngleDoubleDownStyled = styled(AngleDoubleDown)`
