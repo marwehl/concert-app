67bc40382da2aea3d33be3861edc322fd8a0db2b
marwehl <Marie_S@gmx.de>
2019-10-12 22:49:28 +0200

    Add select logic

--- src/Calendar.js
+++ src/Calendar.js
@@ -3,17 +3,38 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from "@fullcalendar/interaction";
 
-export default function Calendar () {
+export default function Calendar ({concerts}) {
+  
+  const dates = concerts.map(concert => concert.fullDate)
+ const slicedDates = dates.map(foo => foo.slice(0,10))
+
+ console.log('concerts',concerts)
+
+  const newEvents = slicedDates.map(slicedDate => { return { title: 'event', date: slicedDate}})
+  const newDate = "2019-10-10T19:00:00.000Z"
+  const selectedConcert = concerts.filter(concert => concert.fullDate === newDate)
+
+  console.log(selectedConcert)
+
   return (
     <FullCalendar
       defaultView="dayGridMonth"
       plugins={[dayGridPlugin, interactionPlugin]}
       weekends={true}
-      events={[
-        { title: 'event 1', date: '2019-10-01' },
-        { title: 'event 2', date: '2019-10-12' }
-      ]}
-      eventClick={()=> console.log('clicked')}
+      events={newEvents}
+      dateClick={(event) => handleClick(event, concerts)}
     />
   )
+
+  function handleClick(event, concerts) {
+    const eventDate = event.date
+   const newDate = eventDate.toISOString().slice(0,10)
+  const selectedConcert = concerts.filter(concert => concert.fullDate.slice(0,10) === newDate)
+  console.log(event)
+  console.log(eventDate)
+  console.log(newDate)
+  console.log(selectedConcert)
+  }
+
+
 }
