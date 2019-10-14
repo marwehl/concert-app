0e9809cb0634e7e9a9274306446c3f2e303182c5
marwehl <Marie_S@gmx.de>
2019-10-14 08:30:37 +0200

    Import popupbox

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Concert from './concert/Concert'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from "@fullcalendar/interaction";
+import {
+  PopupboxManager,
+  PopupboxContainer
+} from 'react-popupbox';
+
 
 export default function Calendar ({concerts}) {
+
+  const [selectedConcert, setSelectedConcert] = useState({})
+  const [activeConcert, setActiveConcert] = useState({})
+
+  console.log('selectedConcert', selectedConcert)
   
   const dates = concerts.map(concert => concert.fullDate)
  const slicedDates = dates.map(foo => foo.slice(0,10))
@@ -11,12 +22,9 @@ export default function Calendar ({concerts}) {
  console.log('concerts',concerts)
 
   const newEvents = slicedDates.map(slicedDate => { return { title: 'event', date: slicedDate}})
-  const newDate = "2019-10-10T19:00:00.000Z"
-  const selectedConcert = concerts.filter(concert => concert.fullDate === newDate)
-
-  console.log(selectedConcert)
 
   return (
+    <>
     <FullCalendar
       defaultView="dayGridMonth"
       plugins={[dayGridPlugin, interactionPlugin]}
@@ -24,16 +32,36 @@ export default function Calendar ({concerts}) {
       events={newEvents}
       dateClick={(event) => handleClick(event, concerts)}
     />
+      <div>
+        <button onClick={openPopupbox}>Click me</button>
+        <PopupboxContainer/>   
+      </div>
+     
+   
+    </>
+
   )
+
+  function openPopupbox(activeConcert) {
+    const content = (
+      <Concert
+        artist={activeConcert.artist} fullDate={activeConcert.fullDate} description={activeConcert.description} genres={['foo', 'bar']} />
+    )
+    PopupboxManager.open({ content })
+  }
+
 
   function handleClick(event, concerts) {
     const eventDate = event.date
    const newDate = eventDate.toISOString().slice(0,10)
-  const selectedConcert = concerts.filter(concert => concert.fullDate.slice(0,10) === newDate)
-  console.log(event)
-  console.log(eventDate)
-  console.log(newDate)
-  console.log(selectedConcert)
+  const activeConcert = concerts.filter(concert => concert.fullDate.slice(0,10) === newDate)
+  setSelectedConcert(activeConcert)
+    console.log('activeConcert', activeConcert)
+    
+    openPopupbox(activeConcert)
+
+    
+
   }
 
 
